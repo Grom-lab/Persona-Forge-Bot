bb520cc2e3c80e4232b5fc6f57ef901342512da6
Albatas <54707947+Grom-lab@users.noreply.github.com>
2025-03-19 12:46:43 +0300

    Update __init__.py

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,1 +1,8 @@
+from .main_handlers import start_handler, toggle_nsfw_handler
+from .form_handlers import form_conversation_handler
 
+__all__ = [
+    'start_handler', 
+    'toggle_nsfw_handler', 
+    'form_conversation_handler'
+]
