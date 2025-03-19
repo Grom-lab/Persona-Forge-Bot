0450fcee9b5bd7ab598ba713999c4ad40cf4c9e8
Albatas <54707947+Grom-lab@users.noreply.github.com>
2025-03-19 12:26:29 +0300

    Update settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,1 +1,11 @@
+from pydantic import BaseSettings
 
+class Settings(BaseSettings):
+    telegram_bot_token: str
+    gemini_api_key: str
+    
+    class Config:
+        env_file = ".env"
+
+def get_settings():
+    return Settings()
